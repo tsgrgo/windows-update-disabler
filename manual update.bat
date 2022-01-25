664bd03fb31f970ed85b039b53a5641851a09c5f
tsgrgo <tasigergo6@gmail.com>
2022-01-25 17:05:04 +0100

    Create manual update.bat

---
+++ manual update.bat
@@ -0,0 +1,10 @@
+@echo off
+if not "%1"=="admin" (powershell start -verb runas '%0' admin & exit /b)
+sc config wuauserv start= auto
+echo.
+echo Enabled Windows Update Service
+echo You can now use Windows Update Assistant to manually install updates.
+echo Dont forget to rerun the disabler after the update!
+echo More info in README
+echo.
+pause
