2a85289ad90bf890ef374a48cae4364a46bf5e51
tsgrgo <tasigergo6@gmail.com>
2023-03-06 01:10:24 +0100

    Refactoring

--- disable updates.bat
+++ disable updates.bat
@@ -1,37 +1,34 @@
-rem Author: tsgrgo
-rem Disable windows update completely
-rem PsExec is required to get system privileges - it should be in this directory
+:: Author: tsgrgo
+:: Completely disable Windows Update
+:: PsExec is required to get system privileges - it should be in this directory
 
 if not "%1"=="admin" (powershell start -verb runas '%0' admin & exit /b)
 if not "%2"=="system" (powershell %~dp0\PsExec.exe /accepteula -i -s -d '%0' admin system & exit /b)
 
-rem Disable update related services
-net stop wuauserv
-net stop UsoSvc
-net stop uhssvc
-net stop WaaSMedicSvc
-sc config wuauserv start= disabled
-sc failure wuauserv reset= 0 actions= ""
-sc config UsoSvc start= disabled
-sc failure UsoSvc reset= 0 actions= ""
-sc config uhssvc start= disabled
-sc failure uhssvc reset= 0 actions= ""
+:: Disable update related services
+for %%i in (wuauserv, UsoSvc, uhssvc, WaaSMedicSvc) do (
+	net stop %%i
+	sc config %%i start= disabled
+	sc failure %%i reset= 0 actions= ""
+)
 
-rem Forcefully disable update medic service
-takeown /f C:\Windows\System32\WaaSMedicSvc.dll && icacls C:\Windows\System32\WaaSMedicSvc.dll /grant *S-1-1-0:F
-rename C:\Windows\System32\WaaSMedicSvc.dll WaaSMedicSvc_BAK.dll
-icacls C:\Windows\System32\WaaSMedicSvc_BAK.dll /setowner "NT SERVICE\TrustedInstaller" && icacls C:\Windows\System32\WaaSMedicSvc_BAK.dll /remove *S-1-1-0
+:: Brute force rename services
+for %%i in (WaaSMedicSvc) do (
+	takeown /f C:\Windows\System32\%%i.dll && icacls C:\Windows\System32\%%i.dll /grant *S-1-1-0:F
+	rename C:\Windows\System32\%%i.dll %%i_BAK.dll
+	icacls C:\Windows\System32\%%i_BAK.dll /setowner "NT SERVICE\TrustedInstaller" && icacls C:\Windows\System32\%%i_BAK.dll /remove *S-1-1-0
+)
+
+:: Update registry
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v Start /t REG_DWORD /d 4 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v FailureActions /t REG_BINARY /d 000000000000000000000000030000001400000000000000c0d4010000000000e09304000000000000000000 /f
-
 reg add "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
 
-rem Delete downloaded update files
+:: Delete downloaded update files
 erase /f /s /q c:\windows\softwaredistribution\*.* && rmdir /s /q c:\windows\softwaredistribution
 
-rem Disable all update related scheduled tasks
+:: Disable all update related scheduled tasks
 powershell -command "Get-ScheduledTask -TaskPath '\Microsoft\Windows\InstallService\*' | Disable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\Windows\UpdateOrchestrator\*' | Disable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\Windows\UpdateAssistant\*' | Disable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\Windows\WaaSMedic\*' | Disable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\Windows\WindowsUpdate\*' | Disable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\WindowsUpdate\*' | Disable-ScheduledTask"
-
 
 echo Finished
 pause
--- reenable updates.bat
+++ reenable updates.bat
@@ -1,25 +1,28 @@
-rem Author: tsgrgo
-rem Reenable windows auto updates (manual update is more recommended)
-rem PsExec is required to get system privileges - it should be in this directory
+:: Author: tsgrgo
+:: Re-enable Windows auto updates and undo all changes by 'disable updates.bat'
+:: PsExec is required to get system privileges - it should be in this directory
 
 if not "%1"=="admin" (powershell start -verb runas '%0' admin & exit /b)
 if not "%2"=="system" (powershell %~dp0\PsExec.exe /accepteula -i -s -d '%0' admin system & exit /b)
 
-rem Enable update related services
+:: Enable update related services
 sc config wuauserv start= auto
 sc config UsoSvc start= auto
 sc config uhssvc start= delayed-auto
 
-rem Enable update medic service
-takeown /f C:\Windows\System32\WaaSMedicSvc_BAK.dll && icacls C:\Windows\System32\WaaSMedicSvc_BAK.dll /grant *S-1-1-0:F
-rename C:\Windows\System32\WaaSMedicSvc_BAK.dll WaaSMedicSvc.dll
-icacls C:\Windows\System32\WaaSMedicSvc.dll /setowner "NT SERVICE\TrustedInstaller" && icacls C:\Windows\System32\WaaSMedicSvc.dll /remove *S-1-1-0
+:: Restore renamed services
+for %%i in (WaaSMedicSvc) do (
+	takeown /f C:\Windows\System32\%%i_BAK.dll && icacls C:\Windows\System32\%%i_BAK.dll /grant *S-1-1-0:F
+	rename C:\Windows\System32\%%i_BAK.dll %%i.dll
+	icacls C:\Windows\System32\%%i.dll /setowner "NT SERVICE\TrustedInstaller" && icacls C:\Windows\System32\%%i.dll /remove *S-1-1-0
+)
+
+:: Update registry
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v Start /t REG_DWORD /d 3 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\WaaSMedicSvc" /v FailureActions /t REG_BINARY /d 840300000000000000000000030000001400000001000000c0d4010001000000e09304000000000000000000 /f
 reg delete "HKLM\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /f
 
-rem Enable all update related scheduled tasks
+:: Enable all update related scheduled tasks
 powershell -command "Get-ScheduledTask -TaskPath '\Microsoft\Windows\InstallService\*' | Enable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\Windows\UpdateOrchestrator\*' | Enable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\Windows\UpdateAssistant\*' | Enable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\Windows\WaaSMedic\*' | Enable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\Windows\WindowsUpdate\*' | Enable-ScheduledTask; Get-ScheduledTask -TaskPath '\Microsoft\WindowsUpdate\*' | Enable-ScheduledTask"
 
 echo Finished
-pause
